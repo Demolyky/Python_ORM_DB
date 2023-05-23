8312a7833cf538e617f5c4281e327e100eb23bf8
Mikhail Volf <misha.volf93@gmail.com>
2023-05-23 20:07:30 +0300

    correcting output

--- main.py
+++ main.py
@@ -16,25 +16,32 @@ def print_db():
     # получаем экземпляр класса Session
     session = Session()
 
-    # Получаем имя издателя от пользователя
-    publisher_name = input('Введите имя издателя: ')
+    # Получаем информацию от пользователя
+    publisher_name = input('Введите имя издателя (оставьте пустым, если не хотите искать по имени): ')
+    publisher_id = input('Введите ID издателя (оставьте пустым, если не хотите искать по ID): ')
 
-    # Выполняем запрос и выводим результаты
-    sales = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale) \
+    # Выполняем запрос и фильтруем результаты
+    query = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale) \
         .join(Book.publisher) \
         .join(Stock, Stock.id_book == Book.id) \
         .join(Sale, Sale.id_stock == Stock.id) \
-        .join(Shop, Shop.id == Stock.id_shop) \
-        .filter(Publisher.name == publisher_name) \
-        .order_by(Sale.date_sale.desc()).all()
+        .join(Shop, Shop.id == Stock.id_shop)
+
+    if publisher_name:
+        query = query.filter(Publisher.name == publisher_name)
+    if publisher_id:
+        query = query.filter(Publisher.id == publisher_id)
+
+    # Выполняем запрос и выводим результаты
+    sales = query.order_by(Sale.date_sale.desc()).all()
 
     for sale in sales:
-        print(f'{sale[0]} | {sale[1]} | {sale[2]} | {sale[3].strftime("%d-%m-%Y")}')
+        print(f'{sale[0]: <40} | {sale[1]: <10} | {sale[2]: <8} | {sale[3].strftime("%d-%m-%Y")}')
 
 
 def main():
     # заполняем БД из Json-файла
-    populate_database_from_json('tests_data.json', url_db)
+    # populate_database_from_json('tests_data.json', url_db)
 
     print_db()
 
